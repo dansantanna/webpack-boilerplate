73c9d43f4cace9c7a6eacc1e13c0ef6c52adab47
Daniel Sant'anna <daniel.santanna@S2IT.CORP>
2020-05-11 11:45:23 -0300

    :tada: first commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <h1>Started</h1>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "app.js"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
